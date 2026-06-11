49541fcfe3fd41055ec1adb38c2700e4e6196bf4
aswins03 <aswinsatheesh03@gmail.com>
2026-06-11 16:05:28 +0530

    just a commit

--- chatbot_logic.py
+++ chatbot_logic.py
@@ -9,7 +9,6 @@ from langchain_chroma import Chroma
 from langchain_aws import ChatBedrock
 from langchain_aws import BedrockEmbeddings
 
-from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
